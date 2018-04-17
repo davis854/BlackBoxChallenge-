caf77f95f5173eb9510af21c80a4fe515e9f6ff7
davis854 <davis854@marshall.edu>
2018-04-17 16:58:48 -0400

    4/17

--- create_secretdirectory_natas5.php
+++ create_secretdirectory_natas5.php
@@ -5,7 +5,7 @@ include 'nataslevel5.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-$path = '/NATASCyberWarGame/Files/secret.txt';
+$path = '/NATASCyberWarGame/SECRET/secret.txt';
 $handle = fopen($_SERVER['DOCUMENT_ROOT'] . $path, 'w+') or die('Cannot open file: ' . $path);
 $data = "/WhatsInHere/";
 fwrite($handle, $data);
